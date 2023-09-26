fac7620f9240e9a15bfadaffa69579e6dd4bb2d8
Kenzie Bottoms <kenziebottoms@gmail.com>
2023-09-26 14:02:17 -0500

    request with body interface

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,6 +1,11 @@
 import { Request, Response, Router } from 'express';
 
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined };
+}
+
 const router = Router();
+
 router.get('/', (req: Request, res: Response) => {
   res.send('Hello there');
 });
@@ -21,9 +26,13 @@ router.get('/login', (req: Request, res: Response) => {
   `);
 });
 
-router.post('/login', (req: Request, res: Response) => {
+router.post('/login', (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
-  res.send(email + password);
+  if (email) {
+    res.send(email.toUpperCase());
+  } else {
+    res.send('You must provide an email');
+  }
 });
 
 export { router };
